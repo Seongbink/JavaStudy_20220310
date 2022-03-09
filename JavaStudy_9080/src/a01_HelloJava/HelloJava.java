c2e860e833561a7b5da8cfe47c26b78c31a088fb
Seongbink <tjdqls2s@naver.com>
2022-03-10 01:05:17 +0900

    첫수업 HelloJava

---
+++ JavaStudy_9080/src/a01_HelloJava/HelloJava.java
@@ -0,0 +1,46 @@
+package a01_HelloJava;
+
+/**
+ * 
+ * 여러줄 주석(클래스 또는 메소드를 설명하는 주석)
+ *
+ */
+
+/*
+ * 여러줄 주석
+ */
+
+
+// 한줄 주석
+
+//주석의 단축키 Ctrl + /
+
+
+
+public class HelloJava {
+
+	/**
+	 * 
+	 * 프로그램의 시작지점
+	 * 
+	 */
+	public static void main(String[] args) {
+//		'문자'=> 'ㅁ'
+//		"문자열"=>  "ㅁㅁㅁㅁㅁㅁ"
+//		\n = 줄바꿈 
+		System.out.println("hello, java");
+		System.out.println("강성빈");
+		System.out.println();
+		
+		System.out.print("이름: ");
+		System.out.println("강성빈");
+		System.out.print("연락처: ");
+		System.out.println("010-9432-9080");
+		System.out.print("주소: ");
+		System.out.println("울산광역시 울주군 언양읍");
+		
+		
+		
+	}
+
+}
